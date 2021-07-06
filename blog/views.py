96497793da49fffcb6128aed8e1d9c2aa109c056
ladamsperez <ladamsperezwork@gmail.com>
2021-07-06 16:34:16 -0400

    CSS – make it pretty!

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+
+
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -5,3 +7,4 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
